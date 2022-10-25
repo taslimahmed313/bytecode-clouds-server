d84b189b9fdaf0b35662405e7b9855219a9bf10d
Taslim <taslimahmed313@gmil.com>
2022-10-25 08:24:44 +0600

    Categories API  Sended to Server

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+const categories = require('./data/category.json');
+
 app.get('/', (req, res) =>{
     res.send('The Bytecode Clouds Courses are Coming !!')
+})
+
+app.get('/categories', (req, res)=>{
+    res.send(categories);
 })
 
 app.listen(port, ()=>{
     console.log("Your Port are : ", port);
 })
+
+module.exports = app;
