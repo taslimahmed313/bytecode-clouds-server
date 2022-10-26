ac62bf0a636da6ac08cffd23fc76a4eed6f951b7
Taslim <taslimahmed313@gmil.com>
2022-10-26 14:41:00 +0600

    Checkout page API data Send on server

--- index.js
+++ index.js
@@ -20,6 +20,17 @@ app.get('/course', (req, res)=>{
     res.send(courses);
 })
 
+app.get('/course/:id', (req, res)=>{
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course.category_id === id);
+    res.send(selectedCourse);
+})
+app.get('/course/checkout/:id', (req, res)=>{
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course._id === id);
+    res.send(selectedCourse);
+})
+
 app.listen(port, ()=>{
     console.log("Your Port are : ", port);
 })
